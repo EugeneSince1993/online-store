76173aba511f0ebf29e8c41eee1ed5af51649690
EugeneSince1993 <jk235@mail.ru>
2022-07-09 19:32:28 +0300

    Sorting component made, ProductItem component made on 50%

--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -1,11 +1,56 @@
 import React from 'react';
 import styles from './ProductList.module.scss';
+import iphone12Red from '../../assets/iphone-12-red.jpg';
 
 export const ProductList = () => {
 
   return (
-    <div>
-      ProductList
+    <div className={styles.productList}>
+      <div className={styles.productItem}>
+        <div className={styles.productItemInner}>
+          <div className={styles.image}>
+            <img src={iphone12Red} />
+          </div>
+          <div className={styles.icons}>
+            <div className={styles.rating}>
+              <i class="fa-solid fa-star"></i>
+              <span>5.0</span>
+            </div>
+            <div className={styles.testimonials}>
+              <i class="fa-solid fa-comment"></i>
+              <span>6</span>
+            </div>
+            <div className={styles.favorites}>
+              <i class="fa-solid fa-heart"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.productItem}>
+        <div className={styles.productItemInner}>
+
+        </div>
+      </div>
+      <div className={styles.productItem}>
+        <div className={styles.productItemInner}>
+          
+        </div>
+      </div>
+      <div className={styles.productItem}>
+        <div className={styles.productItemInner}>
+          
+        </div>
+      </div>
+      <div className={styles.productItem}>
+        <div className={styles.productItemInner}>
+          
+        </div>
+      </div>
+      <div className={styles.productItem}>
+        <div className={styles.productItemInner}>
+          
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Sorting/index.jsx
+++ src/components/Sorting/index.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
-
 import styles from './Sorting.module.scss';
 
 export const Sorting = () => {
 
   return (
-    <div>
-      Sorting
+    <div className={styles.sorting}>
+      <div className={styles.popular}>по популярности</div>
+      <div className={styles.price}>
+        <span>по цене</span>
+        <i className="fa-solid fa-arrow-down-short-wide"></i>
+      </div>
+      <div className={styles.rating}>по рейтингу</div>
     </div>
   );
 };
