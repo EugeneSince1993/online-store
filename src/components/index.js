94202d9b17a2a42c1a467e1bc2ff2b8c49219681
EugeneSince1993 <jk235@mail.ru>
2022-08-19 20:27:43 +0300

    old js files removed

--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { Header } from './Header';
-export { Footer } from './Footer';
-export { Filters } from './Filters';
-export { Sorting } from './Sorting';
-export { ProductList } from './ProductList';
