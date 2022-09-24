d00244ecbd4a6eb07e9d71dd3e87e67832ea5dc3
EugeneSince1993 <jk235@mail.ru>
2022-09-24 16:04:43 +0300

    making the cart functionality

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import { useDispatch } from "react-redux";
 import productReducer from "./product/productSlice";
 import filterReducer from "./filter/filterSlice";
+import cartReducer from "./cart/cartSlice";
 import { setupListeners } from "@reduxjs/toolkit/query";
 
 export const store = configureStore({
   reducer: {
     product: productReducer,
     filter: filterReducer,
+    cart: cartReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
 });
