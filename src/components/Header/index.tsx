e8b3471cce7b4b1592856a23203b2e76f3091f33
EugeneSince1993 <jk235@mail.ru>
2022-10-08 17:54:16 +0300

    desktop ui: header fixed, mobile ui: nav sticky

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import { FC, useEffect, useRef } from 'react';
+import MediaQuery from 'react-responsive';
 import { useSelector } from 'react-redux';
 import classNames from 'classnames';
 import { NavLink, useLocation } from 'react-router-dom';
@@ -32,60 +33,109 @@ export const Header: FC = () => {
   }, [favoriteItems]);
 
   return (
-    <header className={classNames(styles.header, 'bgLightGray')}>
-      <div className={styles.logo}>
-        <NavLink to="/">
-          <img src={onlineStoreLogo} alt="online store" />
-        </NavLink>
-      </div>
-      {(location.pathname !== '/cart' && location.pathname !== '/favorites') && (
-        <div className={styles.search}>
-          <Search />
+    <>
+      <header className={classNames(styles.header, 'bgLightGray')}>
+        <div className={styles.logo}>
+          <NavLink to="/">
+            <img src={onlineStoreLogo} alt="online store" />
+          </NavLink>
         </div>
-      )}
-      <div className={styles.nav}>
-        <nav>
-          <ul>
-            <li>
-              <NavLink to="/cart">
-                <div className={classNames(styles.linkInner, styles.cartLink)}>
-                  <div className={styles.linkGroup}>
-                    <div className={styles.linkInnerText}>
-                      <i className="fa-solid fa-cart-shopping"></i>
-                      <div>Корзина</div>
-                    </div>
-                    {cartTotal > 0 && (
-                      <div className={styles.totalCount}>
-                        <div>{cartTotal}</div>
+        {location.pathname === '/' && (
+          <div className={styles.search}>
+            <Search />
+          </div>
+        )}
+        <MediaQuery minWidth={768}>
+          <div className={styles.nav}>
+            <nav>
+              <ul>
+                <li>
+                  <NavLink to="/cart">
+                    <div className={classNames(styles.linkInner, styles.cartLink)}>
+                      <div className={styles.linkGroup}>
+                        <div className={styles.linkInnerText}>
+                          <i className="fa-solid fa-cart-shopping"></i>
+                          <div>Корзина</div>
+                        </div>
+                        {cartTotal > 0 && (
+                          <div className={styles.totalCount}>
+                            <div>{cartTotal}</div>
+                          </div>
+                        )}
                       </div>
-                    )}
-                  </div>
-                </div>
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/favorites">
-                <div className={classNames(styles.linkInner, styles.favoritesLink)}>
-                  <div className={styles.linkGroup}>
-                    <div className={styles.linkInnerText}>
-                      <i className="fa-solid fa-heart"></i>
-                      <div>Избранное</div>
                     </div>
-                    {favoritesTotal > 0 && (
-                      <div className={styles.totalCount}>
-                        <div>{favoritesTotal}</div>
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/favorites">
+                    <div className={classNames(styles.linkInner, styles.favoritesLink)}>
+                      <div className={styles.linkGroup}>
+                        <div className={styles.linkInnerText}>
+                          <i className="fa-solid fa-heart"></i>
+                          <div>Избранное</div>
+                        </div>
+                        {favoritesTotal > 0 && (
+                          <div className={styles.totalCount}>
+                            <div>{favoritesTotal}</div>
+                          </div>
+                        )}
                       </div>
-                    )}
+                    </div>
+                  </NavLink>
+                </li>
+                <li style={{display: "none"}}>
+                  <a href="#">Войти</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </MediaQuery>
+      </header>
+      <MediaQuery maxWidth={450}>
+        <div className={styles.nav}>
+          <nav>
+            <ul>
+              <li>
+                <NavLink to="/cart">
+                  <div className={classNames(styles.linkInner, styles.cartLink)}>
+                    <div className={styles.linkGroup}>
+                      <div className={styles.linkInnerText}>
+                        <i className="fa-solid fa-cart-shopping"></i>
+                        <div>Корзина</div>
+                      </div>
+                      {cartTotal > 0 && (
+                        <div className={styles.totalCount}>
+                          <div>{cartTotal}</div>
+                        </div>
+                      )}
+                    </div>
                   </div>
-                </div>
-              </NavLink>
-            </li>
-            <li style={{display: "none"}}>
-              <a href="#">Войти</a>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </header>
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/favorites">
+                  <div className={classNames(styles.linkInner, styles.favoritesLink)}>
+                    <div className={styles.linkGroup}>
+                      <div className={styles.linkInnerText}>
+                        <i className="fa-solid fa-heart"></i>
+                        <div>Избранное</div>
+                      </div>
+                      {favoritesTotal > 0 && (
+                        <div className={styles.totalCount}>
+                          <div>{favoritesTotal}</div>
+                        </div>
+                      )}
+                    </div>
+                  </div>
+                </NavLink>
+              </li>
+              <li style={{display: "none"}}>
+                <a href="#">Войти</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </MediaQuery>
+    </>
   );
 };
--- src/pages/Product/index.tsx
+++ src/pages/Product/index.tsx
@@ -11,7 +11,6 @@ import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 import parse from 'html-react-parser';
 import styles from './Product.module.scss';
-import { IProduct } from '../../types/IProduct';
 import { CartItem } from '../../redux/cart/types';
 import { useAppDispatch } from '../../redux/hooks';
 import { addItem } from '../../redux/cart/cartSlice';
@@ -21,39 +20,7 @@ import { addFavoriteItem } from '../../redux/favorites/favoriteSlice';
 export const Product: FC = () => {
   const dispatch = useAppDispatch();
 
-  const initialProduct: IProduct = {
-    id: "0",
-    imageUrl: "https://c.dns-shop.ru/thumb/st1/fit/500/500/c37c5e3b5f8dbf96784d596c30b6cb1a/d97646879f66b4c8f4690107b503240f40a6b731ed6575fa7fbe4f0a7d0d63fc.jpg.webp",
-    images: [
-      "https://c.dns-shop.ru/thumb/st1/fit/500/500/c37c5e3b5f8dbf96784d596c30b6cb1a/d97646879f66b4c8f4690107b503240f40a6b731ed6575fa7fbe4f0a7d0d63fc.jpg.webp",
-      "https://c.dns-shop.ru/thumb/st1/fit/500/500/17e47304999c2353cae08c032b3be8f9/1cae89b6daff59973c2b5d665e3af67dfcbedf285821146b8d56d0d772a1f945.jpg.webp",
-      "https://c.dns-shop.ru/thumb/st1/fit/500/500/8ad5c8a0d50f44f746a955c489392be2/3ea2e901a2228ea8bd477b98c8449add00f11aa4ad4646f98270ac094a50cab7.jpg.webp"
-    ],
-    brand: "Apple",
-    name: "Apple iPhone 12 Mini 64 Гб, красный",
-    price: 60000,
-    rating: 4.8,
-    testimonials: 6,
-    memory: 64,
-    ram: 6,
-    cpuCores: 6,
-    screenSize: 5.5,
-    batteryCapacity: 3000,
-    color: "red",
-    productCode: 24663,
-    specifications: [
-      {"Диагональ экрана, дюйм": "5,5"},
-      {"Объем встроенной памяти": "64 Гб"},
-      {"Объем оперативной памяти": "4 Гб"},
-      {"Количество ядер": "6"},
-      {"Ёмкость аккумулятора, мАч": "3000"},
-      {"Цвет": "красный"}
-    ],
-    description: "product description",
-    shortDesc: "6x(2.99 ГГц), 4 Гб, 1 SIM, OLED, 2340x1080, камера 12+12 Мп, NFC, 5G, GPS, 2227 мА*ч",
-  };
-
-  const [productObj, setProductObj] = useState<IProduct>(initialProduct);
+  const [productObj, setProductObj] = useState<{[key: string]: any}>({});
   const [photoIndex, setPhotoIndex] = useState(0);
   const [isOpen, setIsOpen] = useState(false);
   const { id } = useParams();
@@ -69,9 +36,15 @@ export const Product: FC = () => {
       });
   }, []);
 
-  const galleryImages = productObj.images;
+  const objIsNotEmpty = (obj: any) => {
+    return Object.keys(obj).length !== 0;
+  };
 
-  const divsWithGalImgs = galleryImages.map((el, i) => {
+  const productObjIsNotEmpty = objIsNotEmpty(productObj);
+
+  const galleryImages = productObjIsNotEmpty && productObj.images;
+
+  const divsWithGalImgs = galleryImages && galleryImages.map((el: any, i: number) => {
     return (
       <div onClick={() => setIsOpen(true)} key={i}>
         <img src={galleryImages[i]} />
@@ -79,9 +52,9 @@ export const Product: FC = () => {
     );
   });
 
-  const specs = productObj.specifications;
+  const specs = productObjIsNotEmpty && productObj.specifications;
 
-  const specList = specs.map((el, i) => {
+  const specList = specs && specs.map((el: any, i: number) => {
     let specKeys = Object.keys(el);
 
     return (
@@ -211,7 +184,7 @@ export const Product: FC = () => {
           </TabPanel>
           <TabPanel>
             <div className={styles.productDescription}>
-              {parse(productObj.description)}
+              {productObjIsNotEmpty && parse(productObj.description)}
             </div>
           </TabPanel>
         </Tabs>
